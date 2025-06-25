f68021a5eb2486aba38894f344bb9f89a726fe84
Alexander <miguebeltranbl@gmail.com>
2025-06-24 19:32:52 -0500

    Practica

--- src/main/LogicaClasificacion.java
+++ src/main/LogicaClasificacion.java
@@ -4,6 +4,11 @@ import java.util.*;
 
 public class LogicaClasificacion {
 
+    Stack<Character> pila = new Stack<>();
+    Stack<Integer> pilaI = new Stack<>();
+    StringBuilder invertir = new StringBuilder();
+    LinkedList<Integer> pares = new LinkedList<>();
+    LinkedList<Integer> impares = new LinkedList<>();
     /**
      * Invierte una cadena de texto utilizando una pila (Stack).
      *
@@ -15,7 +20,18 @@ public class LogicaClasificacion {
      *         Salida: "odnuM aloH"
      */
     public String invertirCadena(String texto) {
-        return "";
+        
+
+        for (char c : texto.toCharArray()) {
+            pila.push(c);
+        }
+
+        while(!pila.isEmpty()){
+            invertir.append(pila.pop());
+           
+        }
+
+        return invertir.toString();
     }
 
     /**
@@ -30,7 +46,23 @@ public class LogicaClasificacion {
      *         Salida: true
      */
     public boolean validarSimbolos(String expresion) {
-        return false;
+
+        HashMap<Character,Character> simbolos = new HashMap<>(); 
+            simbolos.put(')','(');
+            simbolos.put('}','{');
+            simbolos.put(']','[');
+        for (char c : expresion.toCharArray()) {
+            if(c=='('||c=='{'||c=='['){
+                pila.push(c);
+            }else if(c==')'||c=='}'||c==']'){
+                if(pila.isEmpty() || pila.pop()!=simbolos.get(c)){
+                    return false;
+                }
+            }
+        }
+
+        return pila.isEmpty();
+
     }
 
     /**
@@ -43,8 +75,15 @@ public class LogicaClasificacion {
      *         Salida: [1, 2, 3, 4]
      */
     public List<Integer> ordenarPila(Stack<Integer> pila) {
-
-        return new ArrayList<>();
+        
+        while(!pila.isEmpty()){
+            int temp = pila.pop();
+            while(!pilaI.isEmpty()&&pilaI.peek()>temp){
+                pila.push(pilaI.pop());
+            }
+            pilaI.push(temp);
+        }
+        return pilaI;
     }
 
     /**
@@ -58,7 +97,16 @@ public class LogicaClasificacion {
      *         Salida: [2, 4, 6, 1, 3, 5]
      */
     public List<Integer> clasificarPorParidad(LinkedList<Integer> original) {
+        for (int i : original) {
+            if(i%2==0){
+                pares.add(i);
+            }else{
+                impares.add(i);
+            }
+        }
+        LinkedList<Integer> resultado = new LinkedList<>(pares);
+        resultado.addAll(impares);
 
-        return new ArrayList<>();
+        return resultado;
     }
 }
